b8227029e153518d91d26a421348bf587ce5b838
Danyellias Manso <56615063+danyelliasmanso@users.noreply.github.com>
2021-01-25 13:26:58 -0300

    Alteração do nome do package Exception para Exceptions

--- Backend/pecacerta-api/src/main/java/com/dlls/pecacerta/api/controller/CategoriaController.java
+++ Backend/pecacerta-api/src/main/java/com/dlls/pecacerta/api/controller/CategoriaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dlls.pecacerta.api.exception.ResourceNotFoundException;
+import com.dlls.pecacerta.api.exceptions.ResourceNotFoundException;
 import com.dlls.pecacerta.api.model.Categoria;
 import com.dlls.pecacerta.api.repositories.CategoriaRepository;
 
--- Backend/pecacerta-api/src/main/java/com/dlls/pecacerta/api/exceptions/ResourceNotFoundException.java
+++ Backend/pecacerta-api/src/main/java/com/dlls/pecacerta/api/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.dlls.pecacerta.api.exception;
+package com.dlls.pecacerta.api.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;

d7f391a1dce4561a86d5654f08778585d03b1d8b
Lucas Moraes <lucasdemoraesc@gmail.com>
2021-01-27 00:16:37 -0300

    Corrige tabela Cliente: Campo tipoCliente

--- Backend/pecacerta-api/src/main/resources/db/migration/V03__CREATE_CLIENTES.sql
+++ Backend/pecacerta-api/src/main/resources/db/migration/V03__CREATE_CLIENTES.sql
@@ -15,4 +15,4 @@ CREATE TABLE clientes (
     cli_telefone VARCHAR(12) NOT NULL
 );
 
-INSERT INTO clientes (cli_nome,cli_tipo_cliente,cli_cpf_cnpj,cli_data_nasc,cli_logradouro,cli_numero,cli_complemento,cli_bairro,cli_cep,cli_cidade,cli_estado,cli_email,cli_telefone) VALUES ('Clio P. Wise','Atendente',54691578064,'2021-06-02','Ap #350-1754 Street',230,'624-3890 Urna St.','783-6687 Elit, St.','74.605-110','Sincelejo','Belize','netus.et.malesuada@commodoauctor.ca','40170706');
+INSERT INTO clientes (cli_nome,cli_tipo_cliente,cli_cpf_cnpj,cli_data_nasc,cli_logradouro,cli_numero,cli_complemento,cli_bairro,cli_cep,cli_cidade,cli_estado,cli_email,cli_telefone) VALUES ('Clio P. Wise','PESSOA_JURIDICA',54691578064,'2021-06-02','Ap #350-1754 Street',230,'624-3890 Urna St.','783-6687 Elit, St.','74.605-110','Sincelejo','Belize','netus.et.malesuada@commodoauctor.ca','40170706');

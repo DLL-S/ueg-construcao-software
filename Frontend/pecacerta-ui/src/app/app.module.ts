6c67aa2d9fc08f084e564c8f5b6879bd49f113f3
Danyellias Manso <56615063+danyelliasmanso@users.noreply.github.com>
2021-02-05 12:28:29 -0300

    Criação da tela de Login

--- Frontend/pecacerta-ui/src/app/app.module.ts
+++ Frontend/pecacerta-ui/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
